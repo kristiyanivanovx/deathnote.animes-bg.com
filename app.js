d1b20fbf08fb188ebfe606517c28f8e9872f96bc
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-08-01 23:34:30 +0300

    improvements
    added bootstrap framework, reworked the navbar and some other things that were awful before :^

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-//  js animation (currently not working .-.)
+// //  js animation (currently not working .-.)
 
-const navbar = document.querySelector(".navbar");
-const tl = new TimelineMax();
+// const navbar = document.querySelector(".navbar");
+// const tl = new TimelineMax();
 
-tl.fromTo(
-    nav, 
-    1, 
-    {height: "0%"}, 
-    {height: '80%', ease: Power2.easeInOut}
-).fromTo(
-    nav, 
-    1.2, 
-    {width: "100%"}, 
-    {width: "80%", ease: Power2.easeInOut}
-)
-.fromTo(navbar, 1.2, {x: "-100%"}, {x:'0%', ease: Power2.easeInOut})
+// tl.fromTo(
+//     nav, 
+//     1, 
+//     {height: "0%"}, 
+//     {height: '80%', ease: Power2.easeInOut}
+// ).fromTo(
+//     nav, 
+//     1.2, 
+//     {width: "100%"}, 
+//     {width: "80%", ease: Power2.easeInOut}
+// )
+// .fromTo(navbar, 1.2, {x: "-100%"}, {x:'0%', ease: Power2.easeInOut})
 
-// js counter
+// // js counter
 
-const countEl = document.getElementById('count');
+// const countEl = document.getElementById('count');
 
-updateVisitCount();
+// updateVisitCount();
 
-function updateVisitCount() {
-    fetch('https://api.countapi.xyz/update/deathnote.animes-bg.com/0b9fb932-0d87-4a02-b8d4-291a00e2d397/?amount=1')
-        .then(res => res.json())
-        .then(res => {
-            countEl.innerHTML = res.value;
-    });
-}
+// function updateVisitCount() {
+//     fetch('https://api.countapi.xyz/update/deathnote.animes-bg.com/0b9fb932-0d87-4a02-b8d4-291a00e2d397/?amount=1')
+//         .then(res => res.json())
+//         .then(res => {
+//             countEl.innerHTML = res.value;
+//     });
+// }
