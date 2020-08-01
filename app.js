5ddbfab1899ff7ce4bdf844b986eb1c51e54b1f9
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-08-01 17:27:54 +0300

    further improvements

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//  js animation (currently not working .-.)
+
 const navbar = document.querySelector(".navbar");
 const tl = new TimelineMax();
 
@@ -13,3 +15,17 @@ tl.fromTo(
     {width: "80%", ease: Power2.easeInOut}
 )
 .fromTo(navbar, 1.2, {x: "-100%"}, {x:'0%', ease: Power2.easeInOut})
+
+// js counter
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch('https://api.countapi.xyz/update/deathnote.animes-bg.com/0b9fb932-0d87-4a02-b8d4-291a00e2d397/?amount=1')
+        .then(res => res.json())
+        .then(res => {
+            countEl.innerHTML = res.value;
+    });
+}
