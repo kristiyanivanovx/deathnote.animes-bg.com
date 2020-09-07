4703c3b82b64560890a885135b1cf2407f4247c5
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-09-08 02:17:50 +0300

    animation changes
    only animate when page is loaded

--- js/app.js
+++ js/app.js
@@ -1,68 +1,70 @@
-// animation text
-anime({
-    targets: ['display-4',
-              '.rules-image',
-              '.rules',
-              '.lead',
-              '.row',
-              '.counter'
-    ],
-    keyframes: [
-        {translateY: -20},
-        {translateY: 0},
-    ],
-    endDelay: 1000,
-    loop: false,
-    easing: 'easeInOutExpo'
-});
-
-anime({
-    targets: ['.link-dn',
-              '.link-sd',
-    ],
-    keyframes: [
-        {translateY: -15},
-        {translateY: 0},
-    ],
-    endDelay: 1000,
-    loop: false,
-    easing: 'easeInOutExpo'
-});
-
-// animation footer text
-anime({
-    targets: ['.foot-text',
-    ],
-    keyframes: [
-        {translateX: -1100},
-        {translateX: 0},
-    ],
-    endDelay: 1000,
-    loop: false,
-    easing: 'easeInOutExpo'
-});
-
-// animation brand text
-anime({
-    targets: ['.navbar-brand',
-    ],
-    keyframes: [
-        {translateY: -1000},
-        {translateY: 0},
-    ],
-    endDelay: 1000,
-    loop: false,
-    easing: 'easeInOutExpo'
-});
-
-anime({
-    targets: ['.text-center',
-    ],
-    keyframes: [
-        {translateY: -10},
-        {translateY: 0},
-    ],
-    endDelay: 1000,
-    loop: false,
-    easing: 'easeInOutExpo'
-});
+function animate(){
+    anime({
+        targets: ['display-4',
+                  '.rules-image',
+                  '.rules',
+                  '.lead',
+                  '.row',
+                  '.counter'
+        ],
+        keyframes: [
+            {translateY: -20},
+            {translateY: 0},
+        ],
+        endDelay: 1000,
+        loop: false,
+        easing: 'easeInOutExpo'
+    });
+    
+    anime({
+        targets: ['.link-dn',
+                  '.link-sd',
+        ],
+        keyframes: [
+            {translateY: -15},
+            {translateY: 0},
+        ],
+        endDelay: 1000,
+        loop: false,
+        easing: 'easeInOutExpo'
+    });
+    
+    // animation footer text
+    anime({
+        targets: ['.foot-text',
+        ],
+        keyframes: [
+            {translateX: -1100},
+            {translateX: 0},
+        ],
+        endDelay: 1000,
+        loop: false,
+        easing: 'easeInOutExpo'
+    });
+    
+    // animation brand text
+    anime({
+        targets: ['.navbar-brand',
+        ],
+        keyframes: [
+            {translateY: -1000},
+            {translateY: 0},
+        ],
+        endDelay: 1000,
+        loop: false,
+        easing: 'easeInOutExpo'
+    });
+    
+    anime({
+        targets: ['.text-center',
+        ],
+        keyframes: [
+            {translateY: -10},
+            {translateY: 0},
+        ],
+        endDelay: 1000,
+        loop: false,
+        easing: 'easeInOutExpo'
+    });
+    
+}
